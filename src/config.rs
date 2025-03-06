5eb6f8d302b0391fb5f43253b77c13d67078e139
rfxy66 <18566129796@163.com>
2025-03-06 17:23:27 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -102,8 +102,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["18566129796.top"];
+pub const RS_PUB_KEY: &str = "aKgVuptUEUvFT4BSRJ8mVmoyM9fa3l24QyNKE0bTnkw=";
 
 pub const RENDEZVOUS_PORT: i32 = 25116;
 pub const RELAY_PORT: i32 = 25117;
