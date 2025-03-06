3a4045752e25048640a6cfb07e2dd13a9b60b3ab
dishuo108 <1083420152@qq.com>
2025-03-06 16:14:44 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
 pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 25116;
+pub const RELAY_PORT: i32 = 25117;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
